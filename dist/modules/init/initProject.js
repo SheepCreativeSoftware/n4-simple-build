783c99452fd164a482fa475072d0b56b94356390
magraina <marinafcegner@gmail.com>
2023-12-03 20:57:00 +0100

    fix: missing lexicon folder in config

--- dist/modules/init/initProject.js
+++ dist/modules/init/initProject.js
@@ -3,11 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.initProject = void 0;
 const storeConfig_1 = require("../config/storeConfig");
 const buntstift_1 = require("buntstift");
+const createBasicFolders_1 = require("./createBasicFolders");
 const initCliPromt_1 = require("./initCliPromt");
 const initProject = async () => {
-    buntstift_1.buntstift.header('Initialize Module');
-    const config = await (0, storeConfig_1.getExistingOrNewConfig)();
-    const newConfig = await (0, initCliPromt_1.initCliPromt)({ config });
-    await (0, storeConfig_1.writeBuildConfig)({ config: newConfig });
+    try {
+        buntstift_1.buntstift.header('Initialize Module');
+        const config = await (0, storeConfig_1.getExistingOrNewConfig)();
+        const newConfig = await (0, initCliPromt_1.initCliPromt)({ config });
+        await (0, storeConfig_1.writeBuildConfig)({ config: newConfig });
+        await (0, createBasicFolders_1.createBasicFolders)({ config: newConfig });
+    }
+    catch (error) {
+        buntstift_1.buntstift.error('Init failed!');
+        if (error instanceof Error)
+            buntstift_1.buntstift.error(JSON.stringify(error));
+    }
 };
 exports.initProject = initProject;
--- src/modules/init/initProject.ts
+++ src/modules/init/initProject.ts
@@ -1,12 +1,19 @@
 import { getExistingOrNewConfig, writeBuildConfig } from '../config/storeConfig';
 import { buntstift } from 'buntstift';
+import { createBasicFolders } from './createBasicFolders';
 import { initCliPromt } from './initCliPromt';
 
 const initProject = async (): Promise<void> => {
-	buntstift.header('Initialize Module');
-	const config = await getExistingOrNewConfig();
-	const newConfig = await initCliPromt({ config });
-	await writeBuildConfig({ config: newConfig });
+	try {
+		buntstift.header('Initialize Module');
+		const config = await getExistingOrNewConfig();
+		const newConfig = await initCliPromt({ config });
+		await writeBuildConfig({ config: newConfig });
+		await createBasicFolders({ config: newConfig });
+	} catch (error) {
+		buntstift.error('Init failed!');
+		if(error instanceof Error) buntstift.error(JSON.stringify(error));
+	}
 };
 
 export { initProject };
